90dcce19171997a032e2c72ff75ae4feb63bc825
INOTJuanca <jucarvajal@unicauca.edu.co>
2023-09-15 21:12:01 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="contenedor">
+    <div class="imagen">
+      <img alt="Vue logo" src="https://pbs.twimg.com/profile_images/1262277252903047169/hpah3oB8_400x400.jpg">
+    </div>
+    <div class="texto">
+      <p>AppMusica</p>
+      <HelloWorld msg="AppMusica" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,8 +26,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+img {
+  width: 6%;
+  padding-right: 100%;
+  flex: 1; /* La imagen ocupará el 50% del ancho disponible */
+  margin-right: 20px; /* Espacio entre la imagen y el texto */
+}
+
+.texto {
+  flex: 1; /* El texto ocupará el 50% del ancho disponible */
 }
 </style>
